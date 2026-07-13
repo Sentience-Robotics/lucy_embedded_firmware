02cae6410dba2044fb68a58899e5a6315bf7d16d
m-brl <103381146+m-brl@users.noreply.github.com>
2026-07-13 14:58:40 +0200

    feat(base): add base
    Signed-off-by: m-brl <103381146+m-brl@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        target = "thumbv6m-none-eabi";
+        rustToolchain = fenix.packages.${system}.combine [
+          fenix.packages.${system}.stable.cargo
+          fenix.packages.${system}.stable.rustc
+          fenix.packages.${system}.stable.clippy
+          fenix.packages.${system}.stable.rustfmt
+          fenix.packages.${system}.stable.rust-src
+          fenix.packages.${system}.targets.${target}.stable.rust-std
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "Lucy Embedded Firmware";
+          packages = [
+            rustToolchain
+            pkgs.flip-link
+            pkgs.probe-rs-tools
+          ];
+          shellHook = ''
+            echo -e ""
+            echo -e "🛡️  \033[1;36mLucy Embedded Firmware\033[1;0m"
+            echo -e "----------------------------"
+            echo -e "cargo: $(cargo --version  | cut -d ' ' -f2)"
+            echo -e "rustc: $(rustc --version | cut -d ' ' -f2)"
+            echo -e "----------------------------"
+          '';
+        };
+      }
+    );
+}

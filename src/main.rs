aa96209339fed0c85be92bdf5ed568bf5ccbedfe
Voxelle <axelle.chypre@gmail.com>
2026-09-01 11:11:29 +0200

    feat(drivers-implementation): started refactoring to take dependencies into account

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-pub mod drivers;
-use crate::{drivers::servo_driver::SG90ModBusAdapter};
-
-fn main() {
-    println!("Starting!");
-    let servo: SG90ModBusAdapter = SG90ModBusAdapter::new(0, 0 ,0 ,0, 0, 0, 0);
-
-
-}

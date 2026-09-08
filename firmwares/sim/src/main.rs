183cccbf3e9ef03bebe7dfb7a5f1a3fc739f3f21
m-brl <103381146+m-brl@users.noreply.github.com>
2026-09-08 11:38:31 +0200

    feat(pc): wip

--- firmwares/sim/src/main.rs
+++ firmwares/sim/src/main.rs
@@ -1,7 +1,71 @@
 use std::io::{Read, Write};
+use std::fs::File;
+use std::fmt;
 use std::time::Duration;
 use modbus_core::{Request, Response, FunctionCode};
 use serialport::{SerialPortType, UsbPortInfo};
+use memmap2::MmapMut;
+
+use lucy_embedded_firmware_core::{
+    modbus::{RegisterTable}
+};
+
+struct RegisterHeader {
+    header: [u8; 32],
+    iterator: u16,
+}
+
+impl RegisterHeader {
+    fn get_register_status(&mut self, register: u16) -> bool {
+        let index: u16 = register / 8;
+        let index2: u16 = register % 8;
+        ((self.header[index as usize] >> (7 - index2)) & 0b1) != 0
+    }
+
+    fn switch_register_status(&mut self, register: u16) {
+        let index: u16 = register / 8;
+        let index2: u16 = register % 8;
+        self.header[index as usize] = self.header[index as usize] ^ ((1 & 0xFF) << (7 - index2))
+    }
+
+    fn set_dirty(&mut self, register: u16) {
+        if self.get_register_status(register) {
+            return
+        }
+        self.switch_register_status(register);
+    }
+
+    fn set_clean(&mut self, register: u16) {
+        if !self.get_register_status(register) {
+            return
+        }
+        self.switch_register_status(register);
+    }
+}
+
+impl fmt::Display for RegisterHeader {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[\n");
+        for tmp in self.header.iter() {
+            write!(f, "{:08b}\n", tmp);
+        }
+        write!(f, "]")
+    }
+}
+
+impl Iterator for RegisterHeader {
+    type Item = (u16, bool);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.iterator < 32 * 8 {
+            let value = (self.iterator, self.get_register_status(self.iterator));
+            self.iterator += 1;
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
 
 fn append_crc(frame: &mut Vec<u8>) {
     let crc = modbus_core::rtu::crc16(&frame);
@@ -25,6 +89,20 @@ fn write_register(
 }
 
 fn main() {
+    /*let shm_path = "/dev/shm/firmware.bin";
+    let mut file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&shm_path).unwrap();
+    let mut mmap = unsafe { MmapMut::map_mut(&file).unwrap() };*/
+
+    let rt = RegisterTable::default();
+    let mut header = RegisterHeader {
+        header: [0u8; 32],
+        iterator: 0,
+    }; 
+
     let target_vid = 0x16c0;
     let target_pid = 0x27dd;
 
@@ -44,9 +122,33 @@ fn main() {
         .timeout(Duration::from_millis(1000))
         .open().unwrap();
 
-    let mut angle: u16 = 180;
+    loop {
+        print!("> ");
+        std::io::stdout().flush();
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let input = input.trim();
 
-    let packet = write_register(0x01, 0x01, angle);
-    let _ = port.write_all(&packet);
-    println!("Packet send: >{:x?}<", packet);
+        let vec: Vec<_> = input.split(',').collect();
+        let reg = vec[0].parse::<u16>().unwrap();
+        let value = vec[1].parse::<u16>().unwrap();
+        rt.registers[reg as usize].set(value);
+        header.set_dirty(reg);
+
+        let mut changes:Vec<u16> = Vec::new();
+        header.iterator = 0;
+        for (iterator, status) in &mut header {
+            if status {
+                changes.push(iterator);
+            }
+        }
+        for register in changes {
+            let packet = write_register(0x01, register, rt.registers[register as usize].get());
+            let _ = port.write_all(&packet);
+            header.set_clean(register);
+            println!("Changes send");
+        }
+    }
 }
